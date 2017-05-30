235bd4c349524b4a5d1c3ae353184b6b9dbc5e20
mawei <mawei.sx@git.com.cn>
2017-05-30 11:15:57 +0800

    build

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -125,7 +125,12 @@ module.exports = {
                 test: /\.(js|jsx)$/,
                 include: paths.appSrc,
                 loader: 'babel',
-
+                query: {
+                    plugins: [
+                        ['import', [{libraryName: "antd", style: 'css'}]],
+                    ],
+                    cacheDirectory: true
+                }
             },
             // The notation here is somewhat confusing.
             // "postcss" loader applies autoprefixer to our CSS.
